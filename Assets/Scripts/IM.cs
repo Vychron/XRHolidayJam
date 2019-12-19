a6d52a378f998badcd58ec2cc53267a05bfc1543
Vychron <21598@ma-web.nl>
2019-12-19 11:12:53 +0100

    created most functionality of the launcher (no visuals yet)

---
+++ Assets/Scripts/IM.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public static class IM {
+    
+    public static bool MouseDown { get {
+            return Input.GetMouseButton(0);
+        }
+    }
+
+    public static bool MouseUp { get {
+            return Input.GetMouseButtonUp(0);
+        }
+    }
+
+    public static Vector2 MousePosition { get {
+            return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+}
